904a796ab41d0a9a63399159479a572bb3c62045
Philipp Jurasic <philipp.jurasic@gmx.de>
2022-06-09 23:16:19 +0200

    finally fixed physarum - off by .5 error

--- physarum/script.js
+++ physarum/script.js
@@ -1,6 +1,8 @@
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById("canvas");
 
-const gl = canvas.getContext("webgl");
+const gl = canvas.getContext("webgl", {
+  antialias: false,
+});
 var frameId;
 enableFloatTextures(gl);
 
@@ -9,32 +11,43 @@ const minimalVertexInfo = twgl.createBufferInfoFromArrays(gl, {
 });
 
 var mouse = [-1, -1];
-var disabled = false, pause;
+var disabled = false,
+  pause;
 
 // Simulation constants
-var simSize = 1024; // width & height of the simulation textures
-if (window.devicePixelRatio > 2)
-  simSize = 512;
+var simSize = 512; // width & height of the simulation textures
+if (window.devicePixelRatio > 2) simSize = 512;
 var pCount = simSize * simSize;
 canvas.width = simSize;
 canvas.height = simSize;
 
 function getRangeVal(id) {
-  return parseFloat(document.getElementById(id).value);
+  var range_val = document.getElementById(id).value;
+  document.getElementById(id + "_out").textContent = range_val;
+  return parseFloat(range_val);
 }
 
 function init() {
   //initialize the particle render shader
-  const rendererInfo = twgl.createProgramInfo(gl, ['vs_points', 'renderShader']);
+  const rendererInfo = twgl.createProgramInfo(gl, [
+    "vs_points",
+    "renderShader",
+  ]);
   const pointsVertexInfo = twgl.createBufferInfoFromArrays(gl, {
-    a_Index: { numComponents: 2, data: initIndices(), },
+    a_Index: {
+      numComponents: 2,
+      data: initIndices(),
+    },
   });
 
-  const initInfo = twgl.createProgramInfo(gl, ['vs', 'initShader']);
-  const textureRendererInfo = twgl.createProgramInfo(gl, ['vs', 'textureRenderShader']);
+  const initInfo = twgl.createProgramInfo(gl, ["vs", "initShader"]);
+  const textureRendererInfo = twgl.createProgramInfo(gl, [
+    "vs",
+    "textureRenderShader",
+  ]);
 
-  const diffusor = new PingPongShader('vs', 'diffusionShader');
-  const simulator = new PingPongShader('vs', 'simShader');
+  const diffusor = new PingPongShader("vs", "diffusionShader");
+  const simulator = new PingPongShader("vs", "simShader");
   diffusor.vertexInfo = minimalVertexInfo;
   simulator.vertexInfo = minimalVertexInfo;
 
@@ -56,23 +69,22 @@ function init() {
       diffusor.uniforms = {
         u_resolution: [gl.canvas.width, gl.canvas.height],
         u_mousePos: mouse,
-        u_evaporationRate: getRangeVal('evaporationrate'),
-      }
+        u_evaporationRate: getRangeVal("evaporationrate"),
+      };
       diffusor.step();
 
       //gl.enable(gl.BLEND);
-      //gl.blendFunc(gl.ONE, gl.ONE);
+      //gl.blendFunc(gl.ONE, gl.ONE_MINUS_DST_ALPHA);
       //update simulation
       simulator.uniforms = {
-        u_dt: .05,
         u_resolution: [gl.canvas.width, gl.canvas.height],
-        u_searchDistance: getRangeVal('searchdistance'),
-        u_speed: getRangeVal('movementspeed'),
-        u_rotationRate: getRangeVal('rotationrate'),
-        u_searchAngle: getRangeVal('searchangle'),
-        u_pheroTexture: diffusor.fb2.attachments[0],
+        u_searchDistance: getRangeVal("searchdistance"),
+        u_speed: getRangeVal("movementspeed"),
+        u_rotationRate: getRangeVal("rotationrate"),
+        u_searchAngle: getRangeVal("searchangle"),
+        u_pheroTexture: diffusor.fb1.attachments[0],
         u_time: (Date.now() - startTime) / 1000,
-      }
+      };
       simulator.step();
       //gl.disable(gl.BLEND);
 
@@ -81,11 +93,10 @@ function init() {
       twgl.setBuffersAndAttributes(gl, textureRendererInfo, minimalVertexInfo);
       twgl.setUniforms(textureRendererInfo, {
         u_resolution: [gl.canvas.width, gl.canvas.height],
-        u_texture: diffusor.bufferTexture,
+        u_texture: diffusor.bufferTexture, // diffusor.fb2.attachments[0]
       });
       twgl.bindFramebufferInfo(gl);
       twgl.drawBufferInfo(gl, minimalVertexInfo);
-
 
       // render the current particles as red dots
       gl.useProgram(rendererInfo.program);
@@ -100,19 +111,18 @@ function init() {
     }
     frameId = requestAnimationFrame(animate);
   }
-  if (!disabled)
-    frameId = requestAnimationFrame(animate);
+  if (!disabled) frameId = requestAnimationFrame(animate);
 }
 
 function initIndices() {
-  // uv positions for sprites in the vertex shader 
+  // uv positions for sprites in the vertex shader
   // (where in the sim texture do the particles get their simulation data from)
-  const simIndex = new Float32Array(~~(pCount * .25));
+  const simIndex = new Float32Array(~~(pCount / 2 ** 2));
   for (let x = 0; x < simSize; x++) {
     for (let y = 0; y < simSize; y++) {
       let i = (x * simSize + y) * 2;
-      simIndex[i] = x * 2 - 1;
-      simIndex[i + 1] = y * 2 - 1;
+      simIndex[i] = (x + 0.5) / simSize;
+      simIndex[i + 1] = (y + 0.5) / simSize;
     }
   }
   return simIndex;
@@ -120,27 +130,33 @@ function initIndices() {
 
 window.onscroll = () => {
   var rect = gl.canvas.getBoundingClientRect();
-  pause = (0 > rect.top + gl.canvas.offsetHeight)
-}
+  pause = 0 > rect.top + gl.canvas.offsetHeight;
+};
 
 class PingPongShader {
   constructor(vs, fs, options = {}) {
-    if ('vertexInfo' in options) {
+    if ("vertexInfo" in options) {
       this.vertexInfo = options.vertexInfo;
     }
-    if ('uniforms' in options) {
+    if ("uniforms" in options) {
       this.uniforms = options.uniforms;
     }
 
     this.shaderInfo = twgl.createProgramInfo(gl, [vs, fs]);
     //initialize the buffer textures
     const attachments = [
-      { format: gl.RGBA, type: gl.FLOAT, wrapS: gl.MIRRORED_REPEAT, wrapT: gl.MIRRORED_REPEAT },
+      {
+        format: gl.RGBA,
+        type: gl.FLOAT,
+        filter: gl.NEAREST,
+        wrapS: gl.CLAMP_TO_EDGE,
+        wrapT: gl.CLAMP_TO_EDGE,
+      },
     ];
     this.fb1 = twgl.createFramebufferInfo(gl, attachments);
     this.fb2 = twgl.createFramebufferInfo(gl, attachments);
-
   }
+
   step() {
     gl.useProgram(this.shaderInfo.program);
     this.uniforms.u_texture = this.fb1.attachments[0];
@@ -149,12 +165,12 @@ class PingPongShader {
     twgl.bindFramebufferInfo(gl, this.fb2);
     twgl.drawBufferInfo(gl, this.vertexInfo);
 
-
     // swap buffers
     var tmp = this.fb1;
     this.fb1 = this.fb2;
     this.fb2 = tmp;
   }
+
   get bufferTexture() {
     return this.fb1.attachments[0];
   }
@@ -162,9 +178,9 @@ class PingPongShader {
 
 document.addEventListener("touchmove", pointermove, false);
 document.addEventListener("mousemove", pointermove, false);
+
 function pointermove(e) {
-  if (e.touches)
-    e = e.touches[0]
+  if (e.touches) e = e.touches[0];
   var rect = gl.canvas.getBoundingClientRect();
   mouse[0] = (e.clientX - rect.left) / gl.canvas.clientWidth;
   mouse[1] = 1 - (e.clientY - rect.top) / gl.canvas.clientHeight;
@@ -172,11 +188,12 @@ function pointermove(e) {
 
 function enableFloatTextures(gl) {
   function webglError() {
-    document.getElementById('webglWarning').style.display = '';
+    document.getElementById("webglWarning").style.display = "";
     disabled = true;
   }
   if (!gl.getExtension("OES_texture_float")) webglError();
   if (!gl.getExtension("OES_texture_float_linear")) webglError();
+  gl.disable(gl.DEPTH_TEST);
 }
 
 init();
